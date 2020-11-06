ea8b237bb9699c5ec413f883d43adbf5f2439c2c
rossini <rossinireyes78@hotmail.com>
2020-11-06 16:35:26 +0100

    guardrestaurant

--- EjercicioRestaurante/src/Restaurant/Restaurante1.java
+++ EjercicioRestaurante/src/Restaurant/Restaurante1.java
@@ -1,32 +1,25 @@
 package Restaurant;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+//import java.util.ArrayList;
 
-
+//import java.util.List;
+//import java.util.Scanner;
 
 public class Restaurante1 {
 
 	public static void main(String[] args) {
-		 int b5=5,b10=10,b20=20,b50=50,b100=100,b200=200,b500=500;
-		 int total = 0;
-		 
-	
-	     //List<Integer> opcion = new ArrayList<>();
-	    
-	     String [] menu = {" Entrantes ", " Spaguettis a la Carbonara ",
-	    		 " Butifarra con Alubias ", " Codillo al Horno ", " Mariscada "};
-	     
-	     int [] precio = {10,18,15,20,35};
-	     
-	     
-	     for(int i=0;i<menu.length;i++){
-	    	 System.out.println((i+1   )+menu[i]+"    " +"€" + precio[i]);
-	       
-	     }
-	     
-	}   
-}
+		// int b5=0,b10=0,b20=0,b50=0,b100=0,b200=0,b500=0;
+		// int total = 0;
 
-	   
+		String[] menu = { " Entrante ", " Spaguettis a la Carbonara ", " Butifarra con Alubias ", " Codillo al Horno ",
+				" Mariscada " };
+
+		int[] precio = { 30, 35, 40, 50, 155 };
+
+		for (int i = 0; i < menu.length; i++) {
+			System.out.println((i + 1) + menu[i] + "    " + "€" + precio[i]);
+
+		}
+
+	}
+}
